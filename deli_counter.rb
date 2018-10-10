2133a2c5f04bbb62d144e27ec9d157aeffd71243
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 17:28:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.size > 0
     line_string = "The line is currently: "
     array.each_with_index {|person, index| line_string << "#{index + 1}. #{person} "}
-    puts line_string.chomp
+    puts line_string.chop
   else
     puts "The line is currently empty."
   end
