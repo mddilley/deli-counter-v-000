e81cb5d3e28e98b2b71c667f5d3cb1fb3fd9d210
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 17:30:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,8 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(array, string)
+  array << string
+  puts "Wecome, #{string}. You are number #{array.size} in line."
+end
