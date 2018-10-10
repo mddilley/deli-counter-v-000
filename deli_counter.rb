f82984fe62e564cc30bc4c039c77aa5ddff457eb
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 17:02:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,8 @@
 katz_deli = []
 
 def line(array, name)
-  array << name
   if array.size > 0
-    puts "Welcome, #{name}. You are number #{array.}"
+    puts "Welcome, #{name}. You are number #{array.index(name) + 1}"
   else
     puts "The line is currently empty."
   end
