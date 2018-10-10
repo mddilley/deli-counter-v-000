e662dfaef1998e571b6a90acbe2c5c9f6e5b537e
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 17:26:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ katz_deli = []
 def line(array)
   if array.size > 0
     line_string = ""
-    array.each_with_index {|person, index| line_string << "#{person}"}
-    puts line
+    array.each {|person| line_string << "#{person}"}
+    puts line_string
   else
     puts "The line is currently empty."
   end
