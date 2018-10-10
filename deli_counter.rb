34ab7a1991d14567c4ea15dd75c3beb32f4dc66a
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 17:01:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+katz_deli = []
+
+def line(array, name)
+  array << name
+  if array.size > 0
+    puts "Welcome, #{name}. You are number #{array.}"
+  else
+    puts "The line is currently empty."
+  end
+end
